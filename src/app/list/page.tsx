42eee6ca187fcad84c04e0a0ea0a642659757c27
kanoaaaa <kanoaborromeo@icloud.com>
2024-10-25 16:12:42 -1000

    remove button implemented

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -5,23 +5,23 @@ import StuffItem from '@/components/StuffItem';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-/** Render a list of stuff for the logged in user. */
+/** Render a list of stuff for the logged-in user. */
 const ListPage = async () => {
   // Protect the page, only logged in users can access it.
   const session = await getServerSession(authOptions);
   loggedInProtectedPage(
     session as {
       user: { email: string; id: string; randomKey: string };
-      // eslint-disable-next-line @typescript-eslint/comma-dangle
     } | null,
   );
+
   const owner = (session && session.user && session.user.email) || '';
   const stuff = await prisma.stuff.findMany({
     where: {
       owner,
     },
   });
-  // console.log(stuff);
+
   return (
     <main>
       <Container id="list" fluid className="py-3">
@@ -35,6 +35,8 @@ const ListPage = async () => {
                   <th>Quantity</th>
                   <th>Condition</th>
                   <th>Actions</th>
+                  {/* Add the Remove column */}
+                  <th>Remove</th>
                 </tr>
               </thead>
               <tbody>
